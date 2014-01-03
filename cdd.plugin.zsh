01cc4635050ffd8b59bc7924274acaa2dee8d664
hyone <hyone.development@gmail.com>
2014-01-03 15:01:40 +0900

    Fix chpwd_functions do not work

--- cdd.plugin.zsh
+++ cdd.plugin.zsh
@@ -8,4 +8,5 @@ source ${0:r:r}
 zstyle ':completion:*' group-name ''
 zstyle ':completion:*:descriptions' format '%F{yellow}Completing %B%d%b%f'
 
+typeset -ga chpwd_functions
 chpwd_functions+=_cdd_chpwd
